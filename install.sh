e8c5423cc52fc9506968a68225e27549b76b1894
Aditya Basu <aditya.ba@directi.com>
2014-08-01 19:56:04 +0530

    Add a install script for a new machine

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Date of creation: 1-Aug-2014, 7:54 PM
+# Author: mitthu
+# Job:
+# ---
+# Run this script after fresh installation, on a new machine.
+
+echo "source ~/.merc" >>~/.bashrc
