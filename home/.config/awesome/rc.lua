39fcb6f378fd7fb4c32494a61d73dc53732b27c0
Aditya Basu <aditya.ba@directi.com>
2014-09-12 17:02:13 +0530

    Add shortcut to lock screen in awesome.

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -287,7 +287,9 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Mod1"    }, "Right",  function () awful.tag.incmwfact( 0.01)    end),
     awful.key({ modkey, "Mod1"    }, "Left",   function () awful.tag.incmwfact(-0.01)    end),
     awful.key({ modkey, "Mod1"    }, "Down",   function () awful.client.incwfact( 0.01)  end),
-    awful.key({ modkey, "Mod1"    }, "Up",     function () awful.client.incwfact(-0.01)  end)
+    awful.key({ modkey, "Mod1"    }, "Up",     function () awful.client.incwfact(-0.01)  end),
+    -- Running programs
+    awful.key({ modkey,           }, "z", function() awful.util.spawn("xtrlock") end)
 )
 
 -- Source: http://awesome.naquadah.org/wiki/FullScreens
