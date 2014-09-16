8d918d9f2c8dc1a07e460382661bc0b5334c2ec5
Aditya Basu <aditya.ba@directi.com>
2014-09-16 19:43:24 +0530

    awesome: Add startup applications and add Tyrannical config for dynamic tag management.

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -52,6 +52,10 @@ terminal = "xfce4-terminal"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
+-- Startup Applications
+awful.util.spawn("nm-applet")
+awful.util.spawn("google-chrome chrome-extension://enfaahabcinohafeakbliimmoholjeip/pingpong/apps/shell/index.html#__platform=CHROME-EXTENSION&finger-print=478ed674-9fae-4088-86e1-90c1fec6259d")
+
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
@@ -87,14 +91,115 @@ end
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {
-	names = {"1) main", "2) terminal", "3) mail", "4) go.to", "5) firefox", 6, 7, 8, 9 },
-	layout = layouts[2]
+tyrannical.tags = {
+    {
+        name        = "1:Term",                 -- Call the tag "Term"
+      --icon        = "/usr/share/pixmaps/xfce4-terminal.xpm", -- Use this icon for the tag
+        init        = true,                   -- Load the tag on startup
+        exclusive   = true,                   -- Refuse any other type of clients (by classes)
+        screen      = {1,2},                  -- create this tag on screen 1 and screen 2
+        layout      = awful.layout.suit.tile, -- use the tile layout
+        exec_once   = {"xfce4-terminal"};
+        instance    = {"dev", "ops"},         -- accept the following instances. this takes precedence over 'class'
+        class       = { --accept the following classes, refuse everything else (because of "exclusive=true")
+            "xterm" , "urxvt" , "aterm","URxvt","XTerm","konsole","terminator","gnome-terminal","xfce4-terminal"
+        }
+    } ,
+    {
+        name        = "2:Firefox",
+        init        = true,
+        exclusive   = false,
+      --icon        = "/usr/share/pixmaps/firefox.png", -- Use this icon for the tag (uncomment with a real path)
+        screen      = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
+        layout      = awful.layout.suit.max,      -- Use the max layout
+        exec_once   = {"firefox"},
+        class = {
+            "Opera"         , "Firefox"        , "Rekonq"    , "Dillo"        , "Arora",
+            "Chromium"      , "nightly"        , "minefield"    }
+    } ,
+    {
+        name        = "3:Chat",
+        init        = true,
+        exclusive   = true,
+      --icon        = "/usr/share/icons/hicolor/16x16/apps/google-chrome.png", -- Use this icon for the tag (uncomment with a real path)
+        screen      = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
+        layout      = awful.layout.suit.max,      -- Use the max layout
+        instance    = {"google-chrome-stable", "chrome"},
+        class = {
+            "Go.to", "Google-chrome-stable", "Google-chrome", "chrome"    }
+    } ,
+    {
+        name = "4:Files",
+        init        = true,
+        exclusive   = true,
+        screen      = 1,
+        layout      = awful.layout.suit.tile,
+        exec_once   = {"thunar"}, --When the tag is accessed for the first time, execute this command
+        class  = {
+            "Thunar", "Konqueror", "Dolphin", "ark", "Nautilus","emelfm"
+        }
+    } ,
+    {
+        name = "5:Develop",
+        init        = true,
+        exclusive   = true,
+        screen      = 1,
+        clone_on    = 2, -- Create a single instance of this tag on screen 1, but also show it on screen 2
+                         -- The tag can be used on both screen, but only one at once
+        layout      = awful.layout.suit.max                          ,
+        class ={
+            "Kate", "KDevelop", "Codeblocks", "Code::Blocks" , "DDD", "kate4"}
+    } ,
+    {
+        name        = "Doc",
+        init        = false, -- This tag wont be created at startup, but will be when one of the
+                             -- client in the "class" section will start. It will be created on
+                             -- the client startup screen
+        exclusive   = true,
+        layout      = awful.layout.suit.max,
+        class       = {
+            "Assistant"     , "Okular"         , "Evince"    , "EPDFviewer"   , "xpdf",
+            "Xpdf"          ,                                        }
+    } ,
 }
-for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag(tags.names, s, tags.layout)
-end
+
+-- Ignore the tag "exclusive" property for the following clients (matched by classes)
+tyrannical.properties.intrusive = {
+    "ksnapshot"     , "pinentry"       , "gtksu"     , "kcalc"        , "xcalc"               ,
+    "feh"           , "Gradient editor", "About KDE" , "Paste Special", "Background color"    ,
+    "kcolorchooser" , "plasmoidviewer" , "Xephyr"    , "kruler"       , "plasmaengineexplorer",
+}
+
+-- Ignore the tiled layout for the matching clients
+tyrannical.properties.floating = {
+    "MPlayer"      , "pinentry"        , "ksnapshot"  , "pinentry"     , "gtksu"          ,
+    "xine"         , "feh"             , "kmix"       , "kcalc"        , "xcalc"          ,
+    "yakuake"      , "Select Color$"   , "kruler"     , "kcolorchooser", "Paste Special"  ,
+    "New Form"     , "Insert Picture"  , "kcharselect", "mythfrontend" , "plasmoidviewer" 
+}
+
+-- Make the matching clients (by classes) on top of the default layout
+tyrannical.properties.ontop = {
+    "Xephyr"       , "ksnapshot"       , "kruler"
+}
+
+-- Force the matching clients (by classes) to be centered on the screen on init
+tyrannical.properties.centered = {
+    "kcalc"
+}
+
+tyrannical.settings.block_children_focus_stealing = true --Block popups ()
+tyrannical.settings.group_children = true --Force popups/dialogs to have the same tags as the parent client
+
+-- Default Config
+-- tags = {
+-- 	names = {"1) main", "2) terminal", "3) mail", "4) go.to", "5) firefox", 6, 7, 8, 9 },
+-- 	layout = layouts[2]
+-- }
+-- for s = 1, screen.count() do
+--     -- Each screen has its own tag table.
+--     tags[s] = awful.tag(tags.names, s, tags.layout)
+-- end
 -- }}}
 
 -- {{{ Menu
