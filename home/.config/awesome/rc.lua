930bcdb72ba07fd8a4fe7078a0e223fd379e5ee6
Aditya Basu <aditya.ba@directi.com>
2014-09-16 20:57:32 +0530

    awesome: Terminal can now be opened in all tags.

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -53,6 +53,8 @@ editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Startup Applications
+-- Kill old instances on awesome config reload
+awful.util.spawn("killall nm-applet")
 awful.util.spawn("nm-applet")
 awful.util.spawn("google-chrome chrome-extension://enfaahabcinohafeakbliimmoholjeip/pingpong/apps/shell/index.html#__platform=CHROME-EXTENSION&finger-print=478ed674-9fae-4088-86e1-90c1fec6259d")
 
@@ -96,14 +98,11 @@ tyrannical.tags = {
         name        = "1:Term",                 -- Call the tag "Term"
       --icon        = "/usr/share/pixmaps/xfce4-terminal.xpm", -- Use this icon for the tag
         init        = true,                   -- Load the tag on startup
-        exclusive   = true,                   -- Refuse any other type of clients (by classes)
+        exclusive   = false,                   -- Refuse any other type of clients (by classes)
+        fallback    = true,
         screen      = {1,2},                  -- create this tag on screen 1 and screen 2
         layout      = awful.layout.suit.tile, -- use the tile layout
-        exec_once   = {"xfce4-terminal"};
-        instance    = {"dev", "ops"},         -- accept the following instances. this takes precedence over 'class'
-        class       = { --accept the following classes, refuse everything else (because of "exclusive=true")
-            "xterm" , "urxvt" , "aterm","URxvt","XTerm","konsole","terminator","gnome-terminal","xfce4-terminal"
-        }
+        exec_once   = {"xfce4-terminal"}
     } ,
     {
         name        = "2:Firefox",
@@ -111,7 +110,7 @@ tyrannical.tags = {
         exclusive   = false,
       --icon        = "/usr/share/pixmaps/firefox.png", -- Use this icon for the tag (uncomment with a real path)
         screen      = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
-        layout      = awful.layout.suit.max,      -- Use the max layout
+        layout      = awful.layout.suit.tile,      -- Use the max layout
         exec_once   = {"firefox"},
         class = {
             "Opera"         , "Firefox"        , "Rekonq"    , "Dillo"        , "Arora",
@@ -120,10 +119,10 @@ tyrannical.tags = {
     {
         name        = "3:Chat",
         init        = true,
-        exclusive   = true,
+        exclusive   = false,
       --icon        = "/usr/share/icons/hicolor/16x16/apps/google-chrome.png", -- Use this icon for the tag (uncomment with a real path)
         screen      = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
-        layout      = awful.layout.suit.max,      -- Use the max layout
+        layout      = awful.layout.suit.tile,      -- Use the max layout
         instance    = {"google-chrome-stable", "chrome"},
         class = {
             "Go.to", "Google-chrome-stable", "Google-chrome", "chrome"    }
@@ -131,7 +130,7 @@ tyrannical.tags = {
     {
         name = "4:Files",
         init        = true,
-        exclusive   = true,
+        exclusive   = false,
         screen      = 1,
         layout      = awful.layout.suit.tile,
         exec_once   = {"thunar"}, --When the tag is accessed for the first time, execute this command
@@ -142,11 +141,11 @@ tyrannical.tags = {
     {
         name = "5:Develop",
         init        = true,
-        exclusive   = true,
+        exclusive   = false,
         screen      = 1,
         clone_on    = 2, -- Create a single instance of this tag on screen 1, but also show it on screen 2
                          -- The tag can be used on both screen, but only one at once
-        layout      = awful.layout.suit.max                          ,
+        layout      = awful.layout.suit.tile,
         class ={
             "Kate", "KDevelop", "Codeblocks", "Code::Blocks" , "DDD", "kate4"}
     } ,
@@ -155,7 +154,7 @@ tyrannical.tags = {
         init        = false, -- This tag wont be created at startup, but will be when one of the
                              -- client in the "class" section will start. It will be created on
                              -- the client startup screen
-        exclusive   = true,
+        exclusive   = false,
         layout      = awful.layout.suit.max,
         class       = {
             "Assistant"     , "Okular"         , "Evince"    , "EPDFviewer"   , "xpdf",
