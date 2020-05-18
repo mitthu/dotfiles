ba77fb8fcb56380689c3e096b404f59677e4ea36
Aditya Basu <aditya.basu@psu.edu>
2020-05-17 21:28:32 -0400

    zim_tidyurl.sh: option for no www and prompt for url
    * get url using prompt if none is selected in Zim
    * option to choose non-www urls

--- home/.me/scripts/zim_tidyurl.sh
+++ home/.me/scripts/zim_tidyurl.sh
@@ -5,17 +5,44 @@
 # Use:
 # - Trim URLs down to domains
 
+APPNAME="Tidy URL Formatter"
 text=$1
+
+# Get URL using prompt if none is provided (or selected in Zim)
+if [ "$text" = "" ]; then
+	text=$(zenity --entry --title "$APPNAME" --text "Enter URL:")
+	[ $? -ne 0 ] && exit 0 # exit if cancel is pressed
+fi
 
 # Extract URL components
 domain=$(echo $text | awk -F[/:] '{print $4}')
+domain_nowwww=$(echo $domain | sed -e 's/www.//')
 l1=$(echo $text | awk -F[/:] '{print $5}')
 
 # Exit if not a url
-[ "$domain" = "" ] && 
-	zenity --error --text "Only URLs can be formatted." &&
+[ "$domain" = "" ] &&
+	zenity --info --no-wrap --title "$APPNAME" --text "Only urls are supported." &&
 	echo -n $text &&
-	exit 1
+	exit 0
+
+# Create tempfile to hold table
+tblfile=$(tempfile)
+
+if [ "$domain" = "$domain_nowwww" ]; then
+	cat >$tblfile <<EOF
+	TRUE  $domain
+	FALSE $domain/$l1
+	FALSE $text
+EOF
+else
+	cat >$tblfile <<EOF
+	FALSE $domain
+	FALSE $domain/$l1
+	TRUE $domain_nowwww
+	FALSE $domain_nowwww/$l1
+	FALSE $text
+EOF
+fi
 
 # Prompt for choosing style
 read -r -d "" INFOTEXT <<EOF
@@ -23,17 +50,18 @@ read -r -d "" INFOTEXT <<EOF
 $text
 EOF
 modtext=$(\
-	zenity --list --radiolist \
-	--title "Format URL" \
+	zenity --list --editable --radiolist --height 300 \
+	--title "$APPNAME" \
 	--text "$INFOTEXT" \
-	--column "" --column "Choose style" \
-	TRUE "$domain" \
-	FALSE "$domain/$l1" \
+	--column "" --column "Choose display style" \
+	$(cat $tblfile)
 )
 
-if [[ $? == 1 ]]; then
-	# Cancel is pressed, then preserve original & quit  
-	echo -n $text
-else # format the modified form
-	echo -n "[[$text|$modtext]]"
+if [ $? -eq 1 ]; then # if cancel is pressed
+	echo -n $text # preserve original
+else
+	echo -n "[[$text|$modtext]]" # style the url
 fi
+
+# Remove tempfile
+rm $tblfile
