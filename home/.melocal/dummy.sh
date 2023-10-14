8b98725721b0b2506986d5bc7cb000aff476165b
Aditya Basu <aditya.basu@psu.edu>
2023-10-14 06:25:02 +0530

    .tmux.conf: set shell if TMUX_SHELL is set

--- home/.melocal/dummy.sh
+++ home/.melocal/dummy.sh
@@ -1,1 +1,9 @@
 # Add machine specific config files to this folder.
+
+# Configure tmux shell
+#export TMUX_SHELL=$(which bash)
+#if [ $(which fish) ]; then
+#	export TMUX_SHELL=$(which fish)
+#	echo "Set tmux shell"
+#fi
+
