4c543f89d02eb0d30bf09467018462bc728326c0
Aditya Basu <ab.aditya.basu@gmail.com>
2025-06-19 14:42:06 -0700

    add atuin.sh support

--- install.sh
+++ install.sh
@@ -21,3 +21,30 @@ vim +BundleInstall +qall </dev/tty
 
 # Run myupdate to add cron entry
 ~/.me/bin/myupdate
+
+# Install atuin.sh
+if ! command -v atuin >/dev/null 2>&1; then
+	curl --proto '=https' --tlsv1.2 -LsSf https://github.com/atuinsh/atuin/releases/latest/download/atuin-installer.sh | sh
+
+    # Remove plugin entry added by atuin to ~/.bashrc
+	cp ~/.bashrc ~/.bashrc.bak
+	awk '{
+        lines[NR] = $0
+    }
+    /.atuin\/bin\/env/ {
+        last = NR
+    }
+    END {
+        for (i = 1; i <= last; i++)
+            print lines[i]
+    }' ~/.bashrc.bak >~/.bashrc
+
+fi
+
+# Start systemd services
+if command -v systemctl >/dev/null 2>&1; then
+    systemctl --user daemon-reload
+    systemctl --user enable atuin.service
+    systemctl --user start atuin.service
+fi
+
